01d88f369b407f36733eb2011f3d199eb5b9e359
Tiago <tiago@gobacklog.com>
2023-07-14 15:09:53 -0300

    chore: ícone de busca

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -7,6 +7,7 @@ import { SharedModule } from '../shared/shared.module';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { FormsModule } from '@angular/forms';
     SharedModule,
     MatInputModule,
     MatFormFieldModule,
+    MatIconModule,
     FormsModule
   ]
 })
