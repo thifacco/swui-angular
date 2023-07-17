7be8d0ffb335dd7ef33d52a6d01ccaed8697db99
Tiago <tiago@gobacklog.com>
2023-07-17 14:29:28 -0300

    chore: home subscription

--- src/app/features/home/views/home/home.component.ts
+++ src/app/features/home/views/home/home.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HomeRepositoryService } from '../../data/repositories/home-repository.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   searchField: string = '';
+  subscription: Subscription = new Subscription;
 
   constructor(private homeService: HomeRepositoryService) {}
 
   ngOnInit(): void {
-    this.homeService.searchPeople().subscribe(data => console.log(data));
+    void this.homeSearchPeople();
+  }
+
+  homeSearchPeople() {
+    this.subscription = this.homeService.searchPeople().subscribe(data => console.log(data));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe;
   }
 }
