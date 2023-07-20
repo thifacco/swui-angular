6eab8d9531ca4daddc010ed7a11264952b0293b2
Tiago <tiago@gobacklog.com>
2023-07-20 10:23:19 -0300

    feat(starships): lista de todas as naves

--- src/app/features/starships/views/starships/starships.component.ts
+++ src/app/features/starships/views/starships/starships.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { StarshipsRepositoryService } from '../../data/starships-repository.service';
+import { map } from 'rxjs';
+import { IStarshipItem } from '../../interfaces/starship';
 
 @Component({
   selector: 'app-starships',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class StarshipsComponent {
 
+  starships: IStarshipItem[] = [];
+
+  starshipsList$ = this.starshipRepositoryService.getAll().pipe(
+    map(data => this.starships = data.results)
+  );
+
+  displayedColumns: string[] = ['name', 'model', 'manufacturer'];
+  
+  constructor(private starshipRepositoryService: StarshipsRepositoryService) {}
 }
