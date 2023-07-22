f6d3d00a0317c2c67afd615e0cddac147ed76f75
Tiago <tiago@gobacklog.com>
2023-07-22 00:46:34 -0300

    refactor: interface Swapi

--- src/app/features/movies/data/repositories/movies-repository.service.ts
+++ src/app/features/movies/data/repositories/movies-repository.service.ts
@@ -3,16 +3,20 @@ import { Injectable } from '@angular/core';
 import { Observable, tap, map, catchError, of } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
 import { IMovie } from '../interfaces/movie';
+import { Swapi } from 'src/app/features/shared/interfaces/swapi';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesRepositoryService {
 
+  private swapi = new Swapi(environment.swapiAPIObject);
+  private resourceKey = 'films';
+
   constructor(private httpClient: HttpClient) { }
 
   listMovies(): Observable<any> {
-    return this.httpClient.get<IMovie>(environment.baseApiFilms).pipe(
+    return this.httpClient.get<IMovie>(this.swapi.getBaseResourceURL(this.resourceKey)).pipe(
       tap(data => console.log(`Encontrados ${data.count} filmes`)),
       map(data => data.results),
       catchError((e) => {
