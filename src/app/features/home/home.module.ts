4bcf77a6d169f95f01aedf5adf0661689ea18bbc
Tiago Facco <thifacco@gmail.com>
2023-07-24 10:52:11 -0300

    chore: organização de componentes compartilhados

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -7,7 +7,6 @@ import { SharedModule } from '../shared/shared.module';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
 import { HomeRepositoryService } from './data/repositories/home-repository.service';
 
 
@@ -21,7 +20,6 @@ import { HomeRepositoryService } from './data/repositories/home-repository.servi
     SharedModule,
     MatInputModule,
     MatFormFieldModule,
-    MatIconModule,
     FormsModule,
     ReactiveFormsModule
   ],
