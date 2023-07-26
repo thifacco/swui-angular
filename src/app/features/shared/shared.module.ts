5402d3d6b28a88029e9d659e943186b7b21b7072
Tiago Facco <thifacco@gmail.com>
2023-07-26 17:00:33 -0300

    chore: cards de naves e filmes na home

--- src/app/features/shared/shared.module.ts
+++ src/app/features/shared/shared.module.ts
@@ -8,8 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTabsModule } from '@angular/material/tabs';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
@@ -27,7 +27,7 @@ import { RouterModule } from '@angular/router';
     MatToolbarModule,
     MatMenuModule,
     MatTabsModule,
-    MatSidenavModule
+    MatCardModule
   ],
   exports: [
     LoadingComponent,
@@ -39,7 +39,7 @@ import { RouterModule } from '@angular/router';
     MatToolbarModule,
     MatMenuModule,
     MatTabsModule,
-    MatSidenavModule
+    MatCardModule
   ]
 })
 export class SharedModule { }
