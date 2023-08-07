066462be0f9ceebfa68e4deb63b4b82a5ab4f298
Tiago Facco <thifacco@gmail.com>
2023-08-07 11:00:58 -0300

    test: cria instância de cada endpoint da swapi

--- src/app/features/shared/data/repositories/swapi-repository.spec.ts
+++ src/app/features/shared/data/repositories/swapi-repository.spec.ts
@@ -2,7 +2,16 @@ import { SwapiRepository } from './swapi-repository';
 import { environment } from 'src/environments/environment';
 
 describe('SwapiRepository', () => {
-  it('should create an instance', () => {
-    expect(new SwapiRepository(environment.swapiAPIObject, '')).toBeTruthy();
+  beforeEach(() => {
+
+  });
+
+  it('should create instance of each resource', () => {
+    expect(new SwapiRepository(environment.swapiAPIObject, 'films')).toBeTruthy();
+    expect(new SwapiRepository(environment.swapiAPIObject, 'people')).toBeTruthy();
+    expect(new SwapiRepository(environment.swapiAPIObject, 'planets')).toBeTruthy();
+    expect(new SwapiRepository(environment.swapiAPIObject, 'species')).toBeTruthy();
+    expect(new SwapiRepository(environment.swapiAPIObject, 'starships')).toBeTruthy();
+    expect(new SwapiRepository(environment.swapiAPIObject, 'vehicles')).toBeTruthy();
   });
 });
