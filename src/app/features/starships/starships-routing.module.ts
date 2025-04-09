fcef5d5bdbaf1ec08d162d400949f37a2c13a3ad
Tiago Facco <thifacco@gmail.com>
2025-04-08 21:26:42 -0300

    Refatorações diretório starships

--- src/app/features/starships/starships-routing.module.ts
+++ src/app/features/starships/starships-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StarshipsComponent } from './views/starships/starships.component';
+import { StarshipsComponent } from './starships.component';
 
 const routes: Routes = [{ path: '', component: StarshipsComponent}];
 
